c3fa6601bdca252b0fecb1f6f29f7eef7f9fc086
Dandan Shan <dandans@epicfail.eecs.umich.edu>
2019-05-22 20:25:36 -0400

    add make short video

--- make_short_videos.py
+++ make_short_videos.py
@@ -1,11 +1,16 @@
-import cv2, os, sys
+import cv2, os, sys, shutil
 from demo_handscore import main_demo
 
-with open("../../modify_dataset_script/selected_100.txt", "r") as f:
-    for video_path in f:
+txt_path = "../../modify_dataset_script/selected_101_200.txt"
+short_video_folder_name = "short_videos_2"
+
+
+with open(txt_path, "r") as f:
+    for vid_index, video_path in enumerate(f):
         video_path, duration = video_path.strip().split(" ")  #../picked_video_mp4_folder/drink_videos/drink_887_bo2XrSymaFA.mp4 5
         
         vid = video_path.split("/")[3].split("_", 2)[2][:-4]
+        video_name = video_path.split("/")[3][:-4]
         video_path = f"../{video_path}"
         
         # write all frames into a floder in dev/shm
@@ -13,9 +18,9 @@ with open("../../modify_dataset_script/selected_100.txt", "r") as f:
         if not os.path.exists(output_folder):
             os.mkdir(output_folder)
         output_path = f"/dev/shm/{vid}/%06d.jpg" 
-#         cmd = f"./../ffmpeg-git-20190514-amd64-static/ffmpeg -i {video_path} {output_path}"
-#         if (os.system(cmd) == 0):
-#             print("success for one video!")
+        cmd = f"./../ffmpeg-git-20190514-amd64-static/ffmpeg -i {video_path} -q:v 2 {output_path}"
+        if (os.system(cmd) == 0):
+            print(f"@@@@@@@@@@@@@@@@@ success extraction for {vid}!")
             
         # faster rcnn
         _, _, num_handfram, hand_num_list = main_demo(output_folder, vid)
@@ -58,20 +63,30 @@ with open("../../modify_dataset_script/selected_100.txt", "r") as f:
                     hand_range_list.append( (ind1, ind2-1) )
             ind1 = ind2 + 1
                 
-        print(f"hand_range_list len = {len(hand_range_list)}\n", hand_range_list)
+        #print(f"hand_range_list len = {len(hand_range_list)}\n")
         
         # ffmpeg the range into short videos
         # ffmpeg -framerate 24 -i Project%03d.png Project.mp4
-        short_video_folder = f"/x/dandans/short_videos/{vid}"
+        short_video_folder = f"/x/dandans/{short_video_folder_name}/{video_name}"
         if not os.path.exists(short_video_folder):
             os.makedirs(short_video_folder)
         for ind, (start, end) in enumerate(hand_range_list):
-            cmd = f"./../ffmpeg-git-20190514-amd64-static/ffmpeg -start_number {start+1:06d} -i {output_folder}/%06d.jpg -vframes {end-start} {short_video_folder}/{vid}_short_{ind}.mp4"
+            cmd = f"./../ffmpeg-git-20190514-amd64-static/ffmpeg -start_number {start+1:06d} -i {output_folder}/%06d.jpg -vframes {end-start} -vf scale=1280:720 {short_video_folder}/{vid}_short_{ind:03d}.mp4"
             if (os.system(cmd) == 0):
-                print(f"success : {cmd}")
+                print(f"hand_range_list len = {len(hand_range_list)}")
+                print(f"@@@@@@@@@@@@@@@@@ success csombination for {vid}: {cmd}")
             
         
         print(video_path, vid, duration)
-        #print(hand_num_list)
         print(len(hand_num_list))
-        break
+        
+        if os.path.exists(output_folder):
+            shutil.rmtree(output_folder)
+        print("Delete the frame_folder ...\n\n\n")
+            
+        print("*" * 50)
+        print(f"Finish no.{vid_index}  {vid}")
+        print(f"txt_path = {txt_path}")
+        print("*" * 50)
+        print("\n\n\n")
+
